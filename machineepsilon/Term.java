daa790613d80f3091667da2259077e2bf87cdc82
Victor Ma <vic.ma@mail.utoronto.ca>
2018-07-24 01:48:05 -0400

    Add constructor and variables for Term

--- machineepsilon/Term.java
+++ machineepsilon/Term.java
@@ -2,4 +2,13 @@ package machineepsilon;
 
 public class Term
 {
+    private int coefficient;
+    private int exponent;
+
+    public Term(int coefficient, int exponent)
+    {
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+    }
+
 }
