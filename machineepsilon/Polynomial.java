49ee5adb8d322b8c9eab6e39e8fc6d70bdf7de90
Victor Ma <vic.ma@mail.utoronto.ca>
2018-07-31 17:30:14 -0400

    Fix Polynomial constructor

--- machineepsilon/Polynomial.java
+++ machineepsilon/Polynomial.java
@@ -13,27 +13,25 @@ public class Polynomial
     public Polynomial(String polynomial)
     {
         polynomial = polynomial.replace(" ", "");
-        ArrayList<String> terms = new ArrayList<>();
+        //ArrayList<String> terms = new ArrayList<>();
         int startIndex = 0;
 
         for (int i = 1; i < polynomial.length(); i++)
         {
             if (polynomial.charAt(i) == '+')
             {
-                terms.add(polynomial.substring(startIndex, i));
+                terms.add(new Term(polynomial.substring(startIndex, i)));
                 startIndex = i+1;
             }
             else if (polynomial.charAt(i) == '-')
             {
-                terms.add(polynomial.substring(startIndex, i));
+                terms.add(new Term(polynomial.substring(startIndex, i)));
                 startIndex = i;
             }
         }
-        terms.add(polynomial.substring(startIndex, polynomial.length()));
-
-        for (int i=0; i < terms.size(); i++)
-            System.out.println(terms.get(i));
+        terms.add(new Term(polynomial.substring(startIndex, polynomial.length())));
     }
+
     public void add(Term term)
     {
         terms.add(term);
@@ -52,5 +50,7 @@ public class Polynomial
 
     public static void main(String args[])
     {
+        Polynomial p = new Polynomial("5x^2 -3x^(7/8) -6x^(6)");
+        p.print();
     }
 }
