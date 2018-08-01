2b84f67a2b1e22ba112af57f6ba1af7360cd287f
Victor Ma <vic.ma@mail.utoronto.ca>
2018-08-01 01:03:24 -0400

    Replace print with toString in Polynomial

--- machineepsilon/Polynomial.java
+++ machineepsilon/Polynomial.java
@@ -13,7 +13,6 @@ public class Polynomial
     public Polynomial(String polynomial)
     {
         polynomial = polynomial.replace(" ", "");
-        //ArrayList<String> terms = new ArrayList<>();
         int startIndex = 0;
 
         for (int i = 1; i < polynomial.length(); i++)
@@ -42,15 +41,28 @@ public class Polynomial
         terms.sort(null);
     }
 
-    public void print()
+    public String toString()
     {
-        for (int i=0; i < terms.size(); i++)
-            System.out.println(terms.get(i));
+        String polynomial = terms.get(0).toString();
+        String term;
+
+        for (int i=1; i < terms.size(); i++)
+        {
+            term = terms.get(i).toString();
+            if (term.equals(""))
+                continue;
+            if (term.charAt(0) == '-')
+                polynomial += " - " + term.substring(1);
+            else
+                polynomial += " + " + term;
+        }
+
+        return polynomial;
     }
 
     public static void main(String args[])
     {
-        Polynomial p = new Polynomial("5x^2 -3x^(7/8) -6x^(6)");
-        p.print();
+        Polynomial p = new Polynomial("-x");
+        System.out.println(p);
     }
 }
